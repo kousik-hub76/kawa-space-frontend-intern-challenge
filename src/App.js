b6136df0d590b60b0089ca8b20d7673ecd8f8d28
Kousik <kousikdafarpur2015@gmail.com>
2021-05-23 16:41:29 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
-import { Router } from 'react-router';
 import './App.css';
 import Cards from './components/Cards';
-import Home from './components/Home';
 import Navbar from './components/Navbar';
 
 function App() {
